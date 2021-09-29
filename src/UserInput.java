5123e1cf3753c48b941f10a96d2df5285291457d
Val <Valfur76@gmail.com>
2021-09-29 22:03:59 +0300

    Renamed variable

--- src/UserInput.java
+++ src/UserInput.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class UserInput {
     public String getString() {
         System.out.print("> ");
-        Scanner getkboard = new Scanner(System.in);
-        return getkboard.nextLine();
+        Scanner getKeyboard = new Scanner(System.in);
+        return getKeyboard.nextLine();
     }
 }
